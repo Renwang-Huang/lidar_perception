03aea2c8d09e76b6fa162f507a06ed5c9a007232
Renwang Huang <RenwangHuangX@gmail.com>
2026-04-30 16:02:19 +0800

    Clean up official comments and refactor the code structure

--- src/rog_map/include/utils/scope_timer.hpp
+++ src/rog_map/include/utils/scope_timer.hpp
@@ -1,25 +1,3 @@
-/**
-* This file is part of ROG-Map
-*
-* Copyright 2024 Yunfan REN, MaRS Lab, University of Hong Kong, <mars.hku.hk>
-* Developed by Yunfan REN <renyf at connect dot hku dot hk>
-* for more information see <https://github.com/hku-mars/ROG-Map>.
-* If you use this code, please cite the respective publications as
-* listed on the above website.
-*
-* ROG-Map is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ROG-Map is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with ROG-Map. If not, see <http://www.gnu.org/licenses/>.
-*/
 #pragma once
 
 #include <chrono>
